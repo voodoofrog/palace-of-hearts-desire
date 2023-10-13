f6b0efe0e473bd1d4ef0ac86110763a118a42691
VoodooFrog <voodoofrog@forgottendream.co.uk>
2023-10-13 23:24:22 +0100

    Initial commit

---
+++ src/module/palace-of-hearts-desire.js
@@ -0,0 +1,50 @@
+const adventureName = "Palace of Heart's Desire";
+const moduleName = 'palace-of-hearts-desire';
+
+/**
+ * welcomeJournal (if set) will automatically be imported and opened after the first activation of a
+ * scene imported from the module compendium.
+ * The name here corresponds to a Journal entry in your compendium and must match exactly (it is case
+ * sensitive).
+ * Set to the following to disable:
+ *   const welcomeJournal = '';
+ */
+const welcomeJournal = '';
+/**
+ * creaturePacks is a list of compendium packs to look in for Actors by name (in prioritised order).
+ * If the creature is not found in the first pack, it will search through each subsequent pack.
+ * The first entry here assumes that you have an Actor pack in your module with the "name" of "actors".
+ * The second entry here lists the DnD5e SRD Monsters compendium that comes with the DnD5e system. Feel
+ * free to delete the 'dnd5e.monsters' reference if you want, or swapping it to any other system compendium
+ * reference.
+ * Set to the following to disable:
+ *   const creaturePacks = [];
+ */
+const creaturePacks = [`${moduleName}.pohd-actors`, 'dnd5e.monsters'];
+/**
+ * journalPacks is a list of compendium packs to look in for Journals by name (in prioritised order).
+ * The first entry here assumes that you have a Journal pack in your module with the "name" of "journals".
+ * Set to the following to disable:
+ *   const journalPacks = [];
+ */
+const journalPacks = [];
+/**
+ * macroPacks is a list of compendium packs to look in for Macros by name (in prioritised order).
+ * The first entry here assumes that you have a Macro pack in your module with the "name" of "macros".
+ * Set to the following to disable:
+ *   const macroPacks = [];
+ */
+const macroPacks = [`${moduleName}.pohd-macros`];
+
+Hooks.once('scenePackerReady', (ScenePacker) => {
+  // Initialise the Scene Packer with your adventure name and module name
+  ScenePacker.Initialise({
+    adventureName,
+    moduleName,
+    creaturePacks,
+    journalPacks,
+    macroPacks,
+    welcomeJournal,
+    allowImportPrompts: true,
+  });
+});
